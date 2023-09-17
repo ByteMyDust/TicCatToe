6ee174d56e444a662343e1ca048da3498682ccb3
Craig Lobo <craiglobo1@gmail.com>
2023-09-17 01:57:36 -0600

    consolidated tictactoe rendering to class

--- src/tic-tac-toe.ts
+++ src/tic-tac-toe.ts
@@ -1,18 +1,96 @@
+import Phaser from "phaser";
 type Cell = 'X' | 'O' | '';
 enum Player {
   X = 'X',
   O = 'O',
 }
 type GameWinner = undefined | 'X' | 'O' | 'DRAW';
+const SPRITE_ASSET_KEY = 'SPRITE_ASSET_KEY';
 
-export default class TicTacToe {
+export default class TicTacToe extends Phaser.GameObjects.Graphics{
   #board: Cell[][] = [];
   #currentPlayerTurn: Player = Player.X;
   #gameWinner: GameWinner = undefined;
   #isGameOver = false;
+  scene: Phaser.Scene;
+  #playerTurnTextGameObject!: Phaser.GameObjects.Text;
 
-  constructor() {
+  constructor(scene) {
+    super(scene)
+    scene.add.existing(this);
+
+    this.scene = scene;
+
     this.#initializeBoard();
+  }
+
+  create(){
+    this.scene.add
+      .text(240, 50, 'Tic-Tac-Toe', {
+        color: 'purple',
+        fontFamily: 'Verdana',
+        fontSize: '42px',
+      })
+      .setOrigin(0.5);
+
+    this.#playerTurnTextGameObject = this.scene.add
+      .text(240, 600, 'X turn', {
+        color: 'black',
+        fontFamily: 'Verdana',
+        fontSize: '22px',
+      })
+      .setOrigin(0.5);
+
+    this.lineStyle(12, 0x3e3e3e);
+    this.lineBetween(170, 120, 170, 540);
+    this.lineBetween(314, 120, 314, 540);
+    this.lineBetween(30, 258, 450, 258);
+    this.lineBetween(30, 402, 450, 402);
+    
+    this.#addGamePiece(0, 0);
+    this.#addGamePiece(0, 1);
+    this.#addGamePiece(0, 2);
+
+    this.#addGamePiece(1, 0);
+    this.#addGamePiece(1, 1);
+    this.#addGamePiece(1, 2);
+
+    this.#addGamePiece(2, 0);
+    this.#addGamePiece(2, 1);
+    this.#addGamePiece(2, 2);
+  }
+
+  #addGamePiece(x: number, y: number): void {
+    const pieceSize = 96;
+    const xPos = 50 + (pieceSize + pieceSize / 2) * y;
+    const yPos = 140 + (pieceSize + pieceSize / 2) * x;
+    const piece = this.scene.add.image(xPos, yPos, SPRITE_ASSET_KEY, 2).setScale(6).setOrigin(0).setInteractive();
+    
+    piece.once(Phaser.Input.Events.POINTER_DOWN as string, () => {
+      if (this.isGameOver) {
+        return;
+      }
+
+      const currentPlayer = this.currentPlayerTurn;
+      this.makeMove(x, y);
+
+      if (currentPlayer === 'X') {
+        piece.setFrame(0);
+      } else {
+        piece.setFrame(1);
+      }
+
+      if (this.isGameOver && this.gameWinner !== 'DRAW') {
+        this.#playerTurnTextGameObject.setText(`${currentPlayer} Won!!`);
+        return;
+      }
+      if (this.isGameOver) {
+        this.#playerTurnTextGameObject.setText(this.gameWinner as string);
+        return;
+      }
+
+      this.#playerTurnTextGameObject.setText(`${this.currentPlayerTurn} turn`);
+    });
   }
 
   get currentPlayerTurn(): Player {
